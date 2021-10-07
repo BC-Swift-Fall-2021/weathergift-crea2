a6d418c717ee0e752e04d382a0edce262cc2906f
crea2 <reacc@bc.edu>
2021-10-07 11:09:37 -0400

    Finished 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Christopher Rea on 10/7/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
