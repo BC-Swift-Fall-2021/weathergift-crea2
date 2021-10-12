7c788563f96712821e19ec1af0fd58b62451c485
crea2 <reacc@bc.edu>
2021-10-12 13:27:36 -0400

    Finished 6.10

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -17,4 +17,49 @@ class WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
+    func getData() {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        
+        print("We are accesing URL")
+        
+        guard let url = URL(string: urlString) else {
+            print("Sould not create url from \(urlString)")
+            return
+        }
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error")
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(" \(json)")
+            } catch {
+                print(" Json error: \(error.localizedDescription)")
+            }
+      }
+        task.resume()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
